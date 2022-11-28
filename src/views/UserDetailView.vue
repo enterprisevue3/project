0e607e6b35ae41e0cc5dd24f822269e8b79a7052
Ctibor Laky <laky@efabrica.sk>
2022-11-28 14:48:20 +0100

    initial

---
+++ src/views/UserDetailView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import UserList from "../Users/UserList.vue";
+import UserDetail from "../Users/UserDetail.vue";
+</script>
+
+<template>
+  <main>
+    <UserDetail />
+    <h3>Other users:</h3>
+    <UserList />
+  </main>
+</template>
---
+++ src/views/UsersView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import UserList from "../Users/UserList.vue";
+</script>
+
+<template>
+  <main>
+    <UserList />
+  </main>
+</template>
